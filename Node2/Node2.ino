cc52c6e029e1aa7ed5d772e83cb7b8886181929e
Stijn Schrauwen <stijnschrauwen1995@hotmail.com>
2015-12-23 21:23:39 +0100

    Node 2 met gemiddelde ipv peaktopeak

--- Node2/Node2.ino
+++ Node2/Node2.ino
@@ -1,13 +1,3 @@
-
-/** RF24Mesh_Example.ino by TMRh20
- *
- * This example sketch shows how to manually configure a node via RF24Mesh, and send data to the
- * master node.
- * The nodes will refresh their network address as soon as a single write fails. This allows the
- * nodes to change position in relation to each other and the master node.
- */
-
-
 #include "RF24.h"
 #include "RF24Network.h"
 #include "RF24Mesh.h"
@@ -31,51 +21,77 @@ RF24Mesh mesh(radio, network);
  **/
 #define nodeID 2
 
+unsigned int Mean = 0;   // peak-to-peak level 
+unsigned int signalMax;        //aanmaken min en max signaalwaarde
+unsigned int signalMin;
+unsigned int sample = 0;       //variabele voor het inlezen van het signaal
 
-uint32_t displayTimer = 0;
-uint32_t analog = 0;
-
-struct payload_t {
-  unsigned long ms;
-  unsigned long counter;
-};
+uint32_t PrevTimer = 0;
 
 void setup() {
 
-  Serial.begin(115200);
-  // Set the nodeID manually
-  mesh.setNodeID(nodeID);
+  Serial.begin(115200); 
+  
+  // Set the nodeID manually each node must have a different ID
+  mesh.setNodeID(nodeID);  
+  
   // Connect to the mesh
   Serial.println(F("Connecting to the mesh..."));
   mesh.begin();
 }
 
 
-
 void loop() {
 
   mesh.update();
 
-  // Send to the master node every halfsecond
-  if (millis() - displayTimer >= 1000) {
-    displayTimer = millis();
-    
-    analog++;
-    
-    if(analog == 256)
-      analog = 0;
-    // Send an 'M' type message containing the current millis()
-    if (!mesh.write(&analog, 'M', sizeof(analog))) {
+  // Send to the master node every 2 seconds
+  if (millis() - PrevTimer >= 2000) {
+    PrevTimer = millis(); //initiele tijd bijhouden zodat we kunnen zien wanneer er 5seconde over zijn
 
+    
+    signalMax = 0;
+    signalMin = 1024;
+    
+    // collect data for 50 ms, zo hier kunnen we het gemiddelde tussen het max en min signaal berekenen. 
+    // ipv misch toevallig net het maximum signaal uit te lezen en als volgt een vertekend beeld te krijgen. 
+   while (millis() - PrevTimer < 50)
+   {
+      sample = analogRead(0);     //uitgangssignaal van de microfoon inlezen
+      if (sample < 1024)          // verwijderen van fictieve signalen
+      {
+         if (sample > signalMax)      //initeel is elk signaal groter als signalmax(signalmax = 0). signalmax zal dus gelijk gesteld worden aan het eerste signaal. 
+         {                            //is het Tweede signaal groter dan het eerste dan zal dit bewaard worden in signal max enz.
+            signalMax = sample;       
+         }
+         else if (sample < signalMin) //is de sample niet groter als signal max gaan we kijken of hij kleiner is dan signalmin. dit is de eerste keer sowieso het geval. 
+         {
+            signalMin = sample;
+         }
+      }
+   }
+   
+   Mean = (signalMax + signalMin)/2;                 // grootste sample en kleinste van elkaar aftrekken om de peak-peak amplitude te bekomen
+   uint32_t volts = ((Mean * 3.3) / 1024)*100;  // convert naar  voltage
+   Serial.println(volts);                             //voltage in serial monitor weergeven
+ 
+      // zend een 'M' type bericht naar de raspberry pi
+    if (!mesh.write(&volts, 'M', sizeof(volts))) 
+  {
       // If a write fails, check connectivity to the mesh network
-      if ( ! mesh.checkConnection() ) {
+      if (! mesh.checkConnection()) 
+    {
         //refresh the network address
         Serial.println("Renewing Address");
         mesh.renewAddress();
-      } else {
+      } 
+    else 
+    {
         Serial.println("Send fail, Test OK");
       }
-    } else {
+    } 
+  else 
+  {
       Serial.println("Send OK: ");
     }
   }
